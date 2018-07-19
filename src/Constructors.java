5f1b4ded7fa5ee721eac3801dde2756451e5237d
patkar <39196647+patkar@users.noreply.github.com>
2018-07-19 15:02:15 +0530

    Add files via upload

---
+++ src/Constructors.java
@@ -0,0 +1,12 @@
+public class Constructors {
+	String name;
+	Constructors(String nm)
+	{
+		name=nm;
+	}
+
+public static void main(String[] args) {
+	Constructors e=new Constructors("Arvind");
+	System.out.println(e.name);
+}
+}
